f0da891dd6607615a96cdbbca2cc94b8e7469e71
Ninezy <241230038@unmuhpnk.ac.id>
2026-07-06 23:12:27 +0700

    Implement database migrations, models, and initial seeders

--- database/seeders/GerakanSeeder.php
+++ database/seeders/GerakanSeeder.php
@@ -9,91 +9,129 @@ class GerakanSeeder extends Seeder
 {
     public function run(): void
     {
-        $gerakan = [
+        $gerakans = [
+
+            [
+                'kategori_id' => 1,
+                'nama' => 'Berdiri Menghadap Kiblat',
+                'urutan' => 1,
+                'deskripsi' => 'Berdiri tegak menghadap kiblat sebagai posisi awal shalat.',
+                'gambar_url' => null,
+                'video_url' => null,
+            ],
+
             [
                 'kategori_id' => 1,
                 'nama' => 'Takbiratul Ihram',
-                'urutan' => 1,
+                'urutan' => 2,
                 'deskripsi' => 'Mengangkat kedua tangan sambil mengucapkan takbir.',
-                'gambar_url' => 'images/takbiratul_ihram.jpg',
-                'video_url' => 'videos/takbiratul_ihram.mp4',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
-                'nama' => 'Berdiri (Qiyam)',
-                'urutan' => 2,
-                'deskripsi' => 'Berdiri tegak sambil membaca Al-Fatihah dan surat pendek.',
-                'gambar_url' => 'images/qiyam.jpg',
-                'video_url' => 'videos/qiyam.mp4',
+                'nama' => 'Bersedekap',
+                'urutan' => 3,
+                'deskripsi' => 'Meletakkan tangan kanan di atas tangan kiri setelah takbiratul ihram.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
+            [
+                'kategori_id' => 1,
+                'nama' => 'Membaca Al-Fatihah dan Surat Pendek',
+                'urutan' => 4,
+                'deskripsi' => 'Membaca Surah Al-Fatihah kemudian dilanjutkan dengan surat pendek.',
+                'gambar_url' => null,
+                'video_url' => null,
+            ],
+
             [
                 'kategori_id' => 1,
                 'nama' => 'Ruku',
-                'urutan' => 3,
-                'deskripsi' => 'Membungkukkan badan dengan posisi punggung lurus.',
-                'gambar_url' => 'images/ruku.jpg',
-                'video_url' => 'videos/ruku.mp4',
+                'urutan' => 5,
+                'deskripsi' => 'Membungkukkan badan hingga punggung lurus dan kedua tangan memegang lutut.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
                 'nama' => "I'tidal",
-                'urutan' => 4,
-                'deskripsi' => 'Berdiri kembali setelah ruku.',
-                'gambar_url' => 'images/itidal.jpg',
-                'video_url' => 'videos/itidal.mp4',
+                'urutan' => 6,
+                'deskripsi' => 'Bangkit dari ruku hingga berdiri tegak.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
-                'nama' => 'Sujud',
-                'urutan' => 5,
-                'deskripsi' => 'Bersujud dengan tujuh anggota badan menyentuh lantai.',
-                'gambar_url' => 'images/sujud.jpg',
-                'video_url' => 'videos/sujud.mp4',
+                'nama' => 'Sujud Pertama',
+                'urutan' => 7,
+                'deskripsi' => 'Bersujud dengan tujuh anggota badan menyentuh tempat sujud.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
                 'nama' => 'Duduk di Antara Dua Sujud',
-                'urutan' => 6,
-                'deskripsi' => 'Duduk di antara dua sujud sambil membaca doa.',
-                'gambar_url' => 'images/duduk_dua_sujud.jpg',
-                'video_url' => 'videos/duduk_dua_sujud.mp4',
+                'urutan' => 8,
+                'deskripsi' => 'Duduk di antara sujud pertama dan kedua sambil membaca doa.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
-                'nama' => 'Tasyahud Awal',
-                'urutan' => 7,
-                'deskripsi' => 'Duduk membaca tasyahud pada rakaat kedua.',
-                'gambar_url' => 'images/tasyahud_awal.jpg',
-                'video_url' => 'videos/tasyahud_awal.mp4',
+                'nama' => 'Sujud Kedua',
+                'urutan' => 9,
+                'deskripsi' => 'Melakukan sujud kedua sebelum berdiri ke rakaat berikutnya.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
                 'nama' => 'Berdiri Rakaat Berikutnya',
-                'urutan' => 8,
-                'deskripsi' => 'Bangkit berdiri untuk melanjutkan rakaat berikutnya.',
-                'gambar_url' => 'images/berdiri_rakaat.jpg',
-                'video_url' => 'videos/berdiri_rakaat.mp4',
+                'urutan' => 10,
+                'deskripsi' => 'Bangkit berdiri untuk memulai rakaat berikutnya.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
+            [
+                'kategori_id' => 1,
+                'nama' => 'Tasyahud Awal',
+                'urutan' => 11,
+                'deskripsi' => 'Duduk membaca tasyahud pada rakaat kedua.',
+                'gambar_url' => null,
+                'video_url' => null,
+            ],
+
             [
                 'kategori_id' => 1,
                 'nama' => 'Tasyahud Akhir',
-                'urutan' => 9,
-                'deskripsi' => 'Duduk membaca tasyahud sebelum salam.',
-                'gambar_url' => 'images/tasyahud_akhir.jpg',
-                'video_url' => 'videos/tasyahud_akhir.mp4',
+                'urutan' => 12,
+                'deskripsi' => 'Duduk membaca tasyahud akhir sebelum salam.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
             [
                 'kategori_id' => 1,
                 'nama' => 'Salam',
-                'urutan' => 10,
-                'deskripsi' => 'Mengucapkan salam ke kanan dan kiri sebagai penutup sholat.',
-                'gambar_url' => 'images/salam.jpg',
-                'video_url' => 'videos/salam.mp4',
+                'urutan' => 13,
+                'deskripsi' => 'Mengucapkan salam ke kanan dan kiri sebagai penutup shalat.',
+                'gambar_url' => null,
+                'video_url' => null,
             ],
+
         ];
 
-        foreach ($gerakan as $item) {
-            Gerakan::create($item);
+        foreach ($gerakans as $gerakan) {
+            Gerakan::create($gerakan);
         }
     }
 }
